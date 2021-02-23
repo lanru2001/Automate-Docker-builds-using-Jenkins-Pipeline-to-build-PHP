566eee6f338d9434cd663b3d1f05faa6215edd47
Devops Coach <kannan.ananth@gmail.com>
2021-02-23 22:56:45 +0000

    modified messages

--- src/index.php
+++ src/index.php
@@ -1,9 +1,3 @@
-
  <?php echo '<p>Hello Folks, it is possible!!</p>'; ?> 
- <?php echo '<p>Welcome to PHP Docker App running inside Docker container!</p>'; ?> 
+ <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
  <?php echo '<p>We have automated Docker image creation and deployment!!!</p>'; ?> 
- <?php echo '<p>We are testing on Ubuntu without installing anything!</p>'; ?> 
- 
- 
- 
-
