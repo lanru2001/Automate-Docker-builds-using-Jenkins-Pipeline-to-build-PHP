00da2643988067f428468f879a35563297abdc54
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-17 13:24:02 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,3 @@
- <?php echo '<p>Hello Folks, it is possible!!</p>'; ?> 
- <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
- <?php echo '<p>I have automated Docker image creation and deployment!!!</p>'; ?> 
+ <?php echo '<p>Welcome to Azeez Temitope Olanrewaju web page </p>'; ?> 
+ <?php echo '<p>Skilled AWS DevOps / Linux Engineer with 3+ years hands-on experience building, deploying and scaling robust AWS infrastructure for quickly growing web applications/p>'; ?> 
+ <?php echo '<p>I have a relentless focus on scalability, automation, optimizing mission critical deployments in AWS, leveraging configuration management and infrastructure as code , CI/CD pipeline and DevOps processes./p>'; ?> 
